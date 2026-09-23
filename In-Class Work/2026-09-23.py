75dbc41ffd2dfc17b84a65c4e5a79cf4cf7fb65e
ivanmorais2552 <ivanmorais2552@gmail.com>
2026-09-23 13:44:31 -0600

    Comit at end of class

--- In-Class Work/2026-09-23.py
+++ In-Class Work/2026-09-23.py
@@ -1,6 +1,12 @@
-# numbers = [4,12,-3,6]
+# numbers = [4, 12, -3, 6]
 
 # for i in numbers:
 #     print(i)
 
-print("This is some code")
+# print("This is some code")
+
+# x = 10 + 5
+
+amount = 100000000
+
+print("If I had a " + "${:,.2f}".format(amount))
